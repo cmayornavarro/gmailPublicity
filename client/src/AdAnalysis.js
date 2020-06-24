7efabd2edd150b00bed4303e9ba16eb572be5809
cmayornavarro <cmayornavarro@gmail.com>
2020-06-24 12:04:13 +0200

    correction on update mount

--- client/src/AdAnalysis.js
+++ client/src/AdAnalysis.js
@@ -1,27 +1,48 @@
 import * as React from "react";
 import LineChart from "./LineChart.js";
+import { Card, CardDeck, Spinner } from "react-bootstrap";
+import "./App.css";
+import optionsSGV from "./svg/options.svg";
+import spamSVG from "./svg/spam.svg";
+import graphSVG from "./svg/graph.svg";
+import emailSVG from "./svg/email.svg";
+
 export default class AdminPage extends React.Component {
   constructor(props) {
     super(props);
-
+    console.log("mygmailAdress");
+    console.log(this.props);
     this.state = {
-      data: props.state.data,
-      mygmailAdress: props.state.mygmailAdress,
-      myToken: props.state.myToken,
-      isAdmin: props.state.isAdmin,
-      name: props.state.name,
+      data: props.data,
+      mygmailAdress: props.mygmailAdress,
+      myToken: props.myToken,
+      isError: false,
+      isSuccess: false,
+      errorMessage: "",
+      successMessage: "",
+      spinnerAnalyse: false,
+      spinnerGetPublicity: false,
+      spinnerGetSpam: false,
     };
   }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
 
+    this.mygmailAdress = this.props.mygmailAdress;
+    this.data = this.props.mygmailAdress;
+    this.myToken = this.props.myToken;
+  }
   componentDidMount() {
+    console.log("componentDidMount");
     this.getMyGmailData();
   }
 
   getMyGmailData = async (e) => {
     if (e) e.preventDefault();
     try {
+      this.setState({ spinnerGetPublicity: true });
       const response = await fetch(
-        "/api/getMyGmailData?mygmailAdress=" + this.state.mygmailAdress
+        "/api/getMyGmailData?mygmailAdress=" + this.mygmailAdress
       );
 
       const body = await response.json();
@@ -37,28 +58,34 @@ export default class AdminPage extends React.Component {
       this.setState({ data: newDataPush });
 
       if (response.status !== 200) throw Error(body.message);
-
+      this.setState({ spinnerGetPublicity: false });
       return body;
     } catch (error) {
+      this.setState({ spinnerGetPublicity: false });
       console.log(error);
     }
   };
 
   gmailDataFetch = async (e) => {
     e.preventDefault();
-    const response = await fetch("/api/fetchGmailData", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ code: this.state.myToken }),
-    });
-    const body = await response.text();
-
-    this.setState({ responseToPost: body });
+    try {
+        this.setState({ spinnerAnalyse: true });
+      const response = await fetch("/api/fetchGmailData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code: this.myToken }),
+      });
+      const body = await response.text();
+      if (response.status !== 200) throw Error(body.message);
+      this.setState({ responseToPost: body, spinnerAnalyse: false });
+    } catch (error) {
+      this.setState({ spinnerAnalyse: false });
+    }
   };
 
-  getMyGmailData = async (e) => {
+  /* getMyGmailData = async (e) => {
     if (e) e.preventDefault();
     try {
       const response = await fetch(
@@ -83,26 +110,113 @@ export default class AdminPage extends React.Component {
     } catch (error) {
       console.log(error);
     }
-  };
+  };*/
 
   render() {
     return (
       <div>
-        <form onSubmit={this.gmailDataFetch}>
-          <p>
-            <strong>Save mails in database</strong>
-          </p>
-
-          <button type="submit">Submit</button>
-        </form>
-
-        <form onSubmit={this.getMyGmailData}>
-          <p>
-            <strong>Get My Gmail Data</strong>
-          </p>
-
-          <button type="submit">Submit</button>
-        </form>
+        <h1 className="spaceBar">
+          Select an option
+          <img
+            src={optionsSGV}
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+            alt="React Bootstrap logo"
+          />
+        </h1>
+        <div className="spaceBar">
+          <CardDeck>
+            <div className="container">
+              <div className="row justify-content-md-center">
+                <div className="col col-lg-4">
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      className="spaceSVG"
+                      src={emailSVG}
+                    />
+                    <Card.Body>
+                      <Card.Title>Analyse my e-mails</Card.Title>
+                      <Card.Text>
+                        This option gets your e-mail publicity and then it
+                        analyses the senders
+                      </Card.Text>
+                      <button
+                        onClick={this.gmailDataFetch}
+                        className="btn btn-primary"
+                      >
+                        Analyse
+                      </button>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">
+                        Available
+                        {this.state.spinnerAnalyse ? (
+                          <Spinner animation="border" variant="danger" />
+                        ) : null}
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                </div>
+                <div className="col col-lg-4">
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      className="spaceSVG"
+                      src={graphSVG}
+                    />
+                    <Card.Body>
+                      <Card.Title>Get my publicity analysis</Card.Title>
+                      <Card.Text>
+                        This options displays the analysis in the graph below
+                      </Card.Text>
+                      <button
+                        onClick={this.getMyGmailData}
+                        className="btn btn-success"
+                      >
+                        Get
+                      </button>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">
+                        Available
+                        {this.state.spinnerGetPublicity ? (
+                          <Spinner animation="border" variant="danger" />
+                        ) : null}
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                </div>
+                <div className="col col-lg-4">
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      className="spaceSVG"
+                      src={spamSVG}
+                    />
+                    <Card.Body>
+                      <Card.Title>Get my spam analysis</Card.Title>
+                      <Card.Text>
+                        This options displays the analysis in the graph below
+                      </Card.Text>
+                      <button
+                        onClick={this.insertDataFetch}
+                        className="btn btn-secondary"
+                        disabled
+                      >
+                        Submit
+                      </button>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">Unavailable</small>
+                    </Card.Footer>
+                  </Card>
+                </div>
+              </div>
+            </div>
+          </CardDeck>
+        </div>
 
         {/*   <GoogleLogout
           clientId="843739110142-765u6gbtq5ip1borpgfkkmvivc3vd3cn.apps.googleusercontent.com"
