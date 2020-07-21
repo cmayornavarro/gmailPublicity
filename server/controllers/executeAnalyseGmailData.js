425a2bf40f1374a4b9b09a577e2ee6cede25a3e3
cmayornavarro <cmayornavarro@gmail.com>
2020-07-21 19:04:01 +0200

    refactoring some code

--- server/controllers/executeAnalyseGmailData.js
+++ server/controllers/executeAnalyseGmailData.js
@@ -14,86 +14,8 @@ const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
 // time.
 const TOKEN_PATH = "token.json";
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-	const { client_secret, client_id, redirect_uris } = credentials.installed;
-	const oAuth2Client = new google.auth.OAuth2(
-		client_id,
-		client_secret,
-		redirect_uris[0]
-	);
-
-	// Check if we have previously stored a token.
-	fs.readFile(TOKEN_PATH, (err, token) => {
-		if (err) return getNewToken(oAuth2Client, callback);
-		oAuth2Client.setCredentials(JSON.parse(token));
-		callback(oAuth2Client);
-	});
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-	const authUrl = oAuth2Client.generateAuthUrl({
-		access_type: "offline",
-		scope: SCOPES,
-	});
-	console.log("Authorize this app by visiting this url:", authUrl);
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
-	rl.question("Enter the code from that page here: ", (code) => {
-		rl.close();
-		oAuth2Client.getToken(code, (err, token) => {
-			if (err) return console.error("Error retrieving access token", err);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-				if (err) return console.error(err);
-				console.log("Token stored to", TOKEN_PATH);
-			});
-			callback(oAuth2Client);
-		});
-	});
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listLabels(auth) {
-	const gmail = google.gmail({ version: "v1", auth });
-	gmail.users.labels.list(
-		{
-			userId: "me",
-		},
-		(err, res) => {
-			if (err) return console.log("The API returned an error: " + err);
-			const labels = res.data.labels;
-			if (labels.length) {
-				console.log("Labels:");
-				labels.forEach((label) => {
-					console.log(`- ${label.name}`);
-				});
-			} else {
-				console.log("No labels found.");
-			}
-		}
-	);
-}
-
-function test(gmail,auth){
+/*This function gets the e-mail adress of the current user*/
+function getEmailAddress(gmail, auth) {
 	var deferred = Q.defer();
 	gmail.users.getProfile(
 		{
@@ -107,17 +29,14 @@ function test(gmail,auth){
 				emailAddress = res.data.emailAddress;
 			}
 			deferred.resolve(emailAddress);
-			
 		}
-	)
+	);
 	return deferred.promise;
 }
 
-
-
- function test2(gmail,auth,emailAddress,nextPage){
+function getEmailsByPage(gmail, auth, emailAddress, nextPage) {
 	var deferred = Q.defer();
-	 var arrayOfPromise = [];
+	var arrayOfPromise = [];
 	gmail.users.messages.list(
 		{
 			userId: "me",
@@ -129,61 +48,58 @@ function test(gmail,auth){
 			const messages = res.data.messages;
 			if (messages.length) {
 				var arrayOfPromises = [];
-				console.log("before ");
-				/*messages.forEach( (message) => {
-				  getEmailFrom(auth, message.id, emailAddress);
-				});*/
-				let promise = messages.map(function(message){
-					getEmailFrom(auth, message.id, emailAddress);
+
+				let promise = messages.map(function (message) {
+					getEmailInformation(auth, message.id, emailAddress);
 					return message;
 				});
-				Promise.all(promise).then(function(){console.log("yes");}).catch(function(err){console.log("err "+ err);}); 
-				console.log("labels found.");
-				//return Q.all(arrayOfPromise);
-				
+				Promise.all(promise)
+					.then(function () {
+						console.log("yes");
+					})
+					.catch(function (err) {
+						console.log("err " + err);
+					});
+				// if there is a next page then get new e-mails
 				if (res.data.nextPageToken) {
 					try {
-						console.log("nextPageToken: " + res.data.nextPageToken);
-						getIdEmails(auth, res.data.nextPageToken);
+						analyseEmails(auth, res.data.nextPageToken);
 					} catch (e) {
 						console.log(e);
 					}
-				}else{
-					 executeDeleteLoadingData(emailAddress);
+				} else {
+					// delete loading data
+					executeDeleteLoadingData(emailAddress);
 				}
-					deferred.resolve("test2 ");
-				
-			} else { 
+				deferred.resolve("test2 ");
+			} else {
 				console.log("No labels found.");
-				
 			}
 		}
 	);
-		return deferred.promise;
+	return deferred.promise;
 }
 
-
- function getIdEmails(auth, nextPage) {
+function analyseEmails(auth, nextPage) {
 	const gmail = google.gmail({ version: "v1", auth });
 	var emailAddress = "";
 	var deferred = Q.defer();
-	
-	test(gmail,auth).then(  function(emailAddress){
-		console.log("callback " + emailAddress);
-		return  test2(gmail,auth,emailAddress,nextPage);
-	}).then(function(a){
-			console.log("callback 2 a:"+a ); 
-			deferred.resolve();
-		}).catch(function(error) {
-            console.log("error "+ error);
-        });
 
-	console.log("deferred.promise");
+	getEmailAddress(gmail, auth)
+		.then(function (emailAddress) {
+			return getEmailsByPage(gmail, auth, emailAddress, nextPage);
+		})
+		.then(function (a) {
+			deferred.resolve();
+		})
+		.catch(function (error) {
+			console.log("error " + error);
+		});
+
 	return deferred.promise;
-	  
 }
 
-async function getEmailFrom(auth, id, emailAddress) {
+async function getEmailInformation(auth, id, emailAddress) {
 	console.log("init");
 	var deferred2 = Q.defer();
 	var arrayOfPromise = [];
@@ -196,13 +112,10 @@ async function getEmailFrom(auth, id, emailAddress) {
 		(err, res) => {
 			if (err) return console.log("The API returned an error: " + err);
 			const headers = res.data.payload.headers;
-		
+
 			if (headers.length) {
 				headers.forEach(async (header) => {
-				
 					if (header.name == "From") {
-						
-						console.log(header.value);
 						const data = {
 							title: "Gmail Pub",
 							tags: ["ReactJS", "NodeJS"],
@@ -213,20 +126,14 @@ async function getEmailFrom(auth, id, emailAddress) {
 							const resp = await insertData(
 								constants.INDEX_ELASTIC,
 								data,
-								res.data.id 
+								res.data.id
 							);
-							
-						} catch (e) { 
+						} catch (e) {
 							console.log(e);
 						}
 					}
-
-					
 				});
-
-				}
-				
-			else {
+			} else {
 				console.log("No labels found.");
 			}
 		}
@@ -235,11 +142,11 @@ async function getEmailFrom(auth, id, emailAddress) {
 	console.log("final ");
 }
 
-const insertData = async function (indexName, data,id) {
+const insertData = async function (indexName, data, id) {
 	console.log(id);
 	return await esClient.index({
 		index: indexName,
-		id:id,// is needed for modify data
+		id: id, // is needed for modify data
 		body: data,
 	});
 };
@@ -253,57 +160,30 @@ var executeGmailData = async function (req, res) {
 		);
 		let emailUser = "";
 		try {
-			 emailUser = req.body.email;
+			emailUser = req.body.email;
 			const data = { emailAddress: emailUser };
-			await insertData(constants.INDEX_ELASTIC_LOADING,data);
-
+			// save the loading data in order to know if there is an analysis on going for the current user
+			await insertData(constants.INDEX_ELASTIC_LOADING, data);
+			// get user's token
 			var newToken = JSON.parse(JSON.stringify(req.body.code));
-			console.log("newToken");
-			console.log(newToken);
-			//var {tokens }= await oauth2Client.getToken(req.body.code);
-
-			//var newToken = JSON.stringify(req.body.code);
 			oauth2Client.setCredentials(newToken);
+			// send a reponse to the client
 			res.send("analyzing data");
-			//oauth2Client.setCredentials(tokens);
-			getIdEmails(oauth2Client).then(function(a){
-				console.log("executeDeleteLoadingData 2 "+a);
-				// executeDeleteLoadingData(emailUser);
-			}).catch(function(err) {
-				console.log("executeDeleteLoadingData ERROR");
-			console.log("err "+err);
-			});
-;
-			
+			// execute the anlysis
+			analyseEmails(oauth2Client)
+				.then(function (a) {
+					console.log("executeDeleteLoadingData 2 " + a);
+					// executeDeleteLoadingData(emailUser);
+				})
+				.catch(function (err) {
+					console.log("error: " + err);
+				});
 		} catch (e) {
 			console.log(e);
-
-		}/*finally{
-			console.log("emailUser "+emailUser  );
-			
-		}*/
+		}
 	} else {
-		// Load client secrets from a local file.
-		fs.readFile("credentials.json", (err, content) => {
-			if (err)
-				return console.log("Error loading client secret file:", err);
-			// Authorize a client with credentials, then call the Gmail API.
-			authorize(JSON.parse(content), getIdEmails);
-		});
+		res.send("token missing");
 	}
 };
-
-/*var executeGmailData = async function (req, res) {
-	const oauth2Client = new google.auth.OAuth2(
-		"843739110142-765u6gbtq5ip1borpgfkkmvivc3vd3cn.apps.googleusercontent.com",
-		"1G-7woSgeEuS33bL7dIGqrL9",
-		"http://localhost:3000/oauth2callback"
-	);
-
-
-	const {tokens} = await oauth2Client.getToken(req.query.code)
-	oauth2Client.setCredentials(tokens);
-	getIdEmails(oauth2Client);
-};*/
 
 module.exports = executeGmailData;
